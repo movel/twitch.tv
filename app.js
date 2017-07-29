5f9ad61cd2281c072ee9f938a3d5bd3d0633ec58
Roman <movel22@gmail.com>
2017-07-29 19:43:43 +0000

    update app.js

--- app.js
+++ app.js
@@ -18,24 +18,15 @@ let channels = [
 let clientId = 'c8a3wkkb56yqjhlcui7tcfyjvs65dy6';
 const url = 'https://api.twitch.tv/kraken/streams/' + channels[0] + '?client_id=' + clientId;
     
-/* global fetch */
-fetch(url)
-  .then((resp) => resp.json()) // Transform the data into json
-  .then(function(data) {
-    console.log(data);
-    
-    })
-  .catch(function(error) {
-    console.log(error);
-  });
-  
-  
 function getChannelInfo() {
   channels.forEach(function(channel) {
     function makeURL(type, name) {
       return 'https://wind-bow.gomix.me/twitch-api/' + type + '/' + name + '?callback=?';
-    };
-    $.getJSON(makeURL("streams", channel), function(data) {
+    }
+    /* global fetch */
+    fetch(makeURL("streams", channel))
+      .then((resp) => resp.json()) // Transform the data into json
+      .then(function(data) {
       var game,
           status;
       if (data.stream === null) {
@@ -47,12 +38,14 @@ function getChannelInfo() {
       } else {
         game = data.stream.game;
         status = "online";
-      };
-      $.getJSON(makeURL("channels", channel), function(data) {
-        var logo = data.logo != null ? data.logo : "https://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F",
+      }
+      fetch(makeURL("channels", channel))
+      .then((resp) => resp.json()) // Transform the data into json
+      .then(function(data) {
+        let logo = data.logo != null ? data.logo : "https://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F",
           name = data.display_name != null ? data.display_name : channel,
           description = status === "online" ? ': ' + data.status : "";
-          html = '<div class="row ' + 
+          let html = '<div class="row ' + 
           status + '"><div class="col-xs-2 col-sm-1" id="icon"><img src="' + 
           logo + '" class="logo"></div><div class="col-xs-10 col-sm-3" id="name"><a href="' + 
           data.url + '" target="_blank">' + 
@@ -63,9 +56,9 @@ function getChannelInfo() {
       });
     });
   });
-};
+}
 
-$(document).ready(function() {
+window.onload = function() {
   getChannelInfo();
   $(".selector").click(function() {
     $(".selector").removeClass("active");
@@ -81,4 +74,4 @@ $(document).ready(function() {
       $(".online").addClass("hidden");
     }
   })
-});
+};
